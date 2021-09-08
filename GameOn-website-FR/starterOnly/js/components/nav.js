b0590362b23ebbba7a7ba79e2c6469d0b323d14c
Johan <johanbn83@gmail.com>
2021-09-09 01:57:25 +0200

    formulaire opérationnel

--- GameOn-website-FR/starterOnly/js/components/nav.js
+++
@@ -1,9 +0,0 @@
-// Nav responsive
-function editNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}

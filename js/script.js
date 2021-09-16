b7ce637452ef2c540e204a0a8848cea8877e3305
johandev <60623961+johandev65@users.noreply.github.com>
2021-09-16 13:52:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-function toggleMenu () {  
     const navbar = document.querySelector('.navbar');
     const burger = document.querySelector('.burger');
     burger.addEventListener('click', (e) => {    
       navbar.classList.toggle('show-nav');
     });    
-  }
-  toggleMenu();
 
 
   // Ciblage des éléments du DOM

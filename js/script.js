bdba373d3fc6dc9d5be92bd1e5ad3e6ae6b30601
johandev <60623961+johandev65@users.noreply.github.com>
2021-09-17 16:13:01 +0200

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,54 +1,66 @@
-    const navbar = document.querySelector('.navbar');
-    const burger = document.querySelector('.burger');
-    burger.addEventListener('click', (e) => {    
-      navbar.classList.toggle('show-nav');
-    });    
+// Menu responsive
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
 
 
-  // Ciblage des éléments du DOM
-  const modalBtn = document.querySelectorAll(".modal-btn");
-  const modalbg = document.querySelector(".bground");
-  const modalbody = document.querySelector(".modal-body");
-  const closemodal = document.querySelector(".close");
-  const closemodalconfirm = document.querySelector(".close2");
 
-  const formData = document.querySelectorAll(".formData input");
-  const pushform = document.querySelector(".btn-submit");
-  const locationDiv = document.querySelector(".location_form");
-  const locationCheckbox = document.querySelectorAll(".location_form [name=\"location\"]");
-  const modalbodyconfirm = document.querySelector(".modal-body-confirm");
+// Ciblage des éléments du DOM de la fenêtre modale et du formulaire
+const modalBtn = document.querySelectorAll(".modal-btn");
+const modalbg = document.querySelector(".bground");
+const modalbody = document.querySelector(".modal-body");
+const modalbodyconfirm = document.querySelector(".modal-body-confirm");
+const closemodal = document.querySelector(".close");
+const closemodalconfirm = document.querySelector(".close-confirm");
+
+const formData = document.querySelectorAll(".formData input");
+const pushform = document.querySelector(".btn-submit");
+const locationDiv = document.querySelector(".location_form");
+const locationCheckbox = document.querySelectorAll(".location_form [name=\"location\"]");
 
 
-  // Evènements
-  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-  closemodal.addEventListener("click", closeModal);
-  closemodal.reset("click", closeModal);
-  closemodalconfirm.addEventListener("click", closeModal);
-  pushform.addEventListener("click", pushForm);
+// Ajout d'évènements
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal)); //ouverture de la modale au clic du bouton "je m'inscris"
+closemodal.addEventListener("click", closeModal); //fermeture de la modale au clic du bouton croix
+closemodal.reset("click", closeModal); //réinitialisation de la modale au clic du bouton croix
+closemodalconfirm.addEventListener("click", closeModal); //fermeture de la modale de confirmation au clic du bouton croix
+pushform.addEventListener("click", pushForm); //validation du formulaire au clic du bouton "c'est parti"
   
-  // lancement de la fenêtre modale
-  function launchModal() {
+
+// lancement de la fenêtre modale
+function launchModal() {
     modalbg.style.display = "block";
-  }
+}
 
 
-  // fermeture des 2 fenêtres modales (celle du formulaire et celle de la confirmation)
-  function closeModal() {
+// fermeture des 2 fenêtres modales (celle du formulaire et celle de la confirmation)
+function closeModal() {
     modalbg.style.display = "none";
     modalbody.style.display = "block";
     modalbodyconfirm.style.display = "none";
     resetForm();
-  }
+}
 
 
-  // validation du formulaire ou non (cela bloquera la soummission si tout n'est pas validé)
-  function pushForm(e) {
+// validation du formulaire ou non (cela bloquera la soummission si tout n'est pas validé)
+function pushForm(e) {
     e.preventDefault();
     validationForm();
-  }
+}
 
-
-  function validationForm() {
+function validationForm() {
     let validation_list = []
     first_data = firstData(formData[0].value);
     last_data = lastData(formData[1].value);
@@ -58,6 +70,7 @@
     location_data = locationData(locationCheckbox);
     condition_data = conditionData(formData[11]);
     validation_form = true;
+
     validation_list.push(first_data, last_data, email_data, birthdate_data, quantity_data, location_data, condition_data);
     for (element in validation_list) {
         // si un ou plusieurs éléments sonts incorrects, cela bloquera la soumission l'envoi du formulaire
@@ -65,162 +78,174 @@
             validation_form = false;
         }
     }
-        // si tous les éléments sont corrects, cela validera la soummission du formulaire et affichera une 2ème fenêtre modale de confirmation)
-        if (validation_form == true) {
+    // si tous les éléments sont corrects, cela validera la soummission du formulaire et affichera une 2ème fenêtre modale de confirmation)
+    if (validation_form == true) {
         modalbody.style.display = "none";
         modalbodyconfirm.style.display = "block";
         resetForm();
-        }
-         // si réponse vide, cela bloquera également la soumission l'envoi du formulaire
-        else {
+    }
+    // si réponse vide, cela bloquera également la soumission l'envoi du formulaire
+    else {
         return false;
     }
-  }
+}
 
-  // Reset du formulaire
-  function resetForm() {
+// Réinitialisation du formulaire
+function resetForm() {
     for (element in formData) {
-      try {    
-        document.getElementById("myForm").reset(); 
-        formData[element].value = "";
-        formData[element].style.borderColor = "white";
-        // reset des messages d'erreurs
-        error_msg_list = ["error1", "error2", "error3", "error4", "error5", "error6", "error7"];
-        var errorData = document.getElementById(error_msg_list[element]);
-        errorData.remove();
-      } catch {}
+        try {    
+            document.getElementById("myForm").reset(); 
+            formData[element].value = "";
+            formData[element].style.borderColor = "white";
+
+            // reset des messages d'erreurs
+            error_msg_list = ["error1", "error2", "error3", "error4", "error5", "error6", "error7"];
+            var errorData = document.getElementById(error_msg_list[element]);
+            errorData.remove();
+        } catch {}
     }
-  }
+}
   
-  // validation du prénom
-  function firstData(data) {
+// validation du prénom
+function firstData(data) {
     var valid = false;
     var error = ('<span id="error1" class="msg_error">Veuillez entrer 2 caractères ou plus pour le champ du nom.</span>');
     var errorData = document.getElementById("error1");
+
     if (data.length > 1) {
-      valid = true;
-      formData[0].style.borderColor = "green";
+        valid = true;
+        formData[0].style.borderColor = "green";
     }
     else {
-      valid = false
-      formData[0].style.borderColor = "red";
-      formData[0].insertAdjacentHTML("afterend", error)
+        valid = false
+        formData[0].style.borderColor = "red";
+        formData[0].insertAdjacentHTML("afterend", error)
     } try {
-      errorData.remove();
+        errorData.remove();
     } catch {}
-    return valid;
-  }
+        return valid;
+}
 
-  // validation du nom
-  function lastData(data) {
+// validation du nom
+function lastData(data) {
     var valid = false;
     var error = ('<span id="error2" class="msg_error">Veuillez entrer 2 caractères ou plus pour le champ du nom.</span>');
     var errorData = document.getElementById("error2");
+
     if (data.length > 1) {
       valid = true;
       formData[1].style.borderColor = "green";
     }
     else {
-      valid = false
-      formData[1].style.borderColor = "red";
-      formData[1].insertAdjacentHTML("afterend", error);
+        valid = false
+        formData[1].style.borderColor = "red";
+        formData[1].insertAdjacentHTML("afterend", error);
     } try {
-      errorData.remove();
+        errorData.remove();
     } catch {}
-    return valid;
+        return valid;
   }
 
-  // validation de l'email
-  function emailData(data) {
+// validation de l'email
+function emailData(data) {
     var valid = false;
     const regex_mail = RegExp(/^[a-z0-9._-]+@[a-z_]+?\.[a-z]{2,3}$/);
     const regex_is = regex_mail.test(data);
     var error = ('<span id="error3" class="msg_error">Vous devez entrer une adresse email valide.</span>');
     var errorData = document.getElementById("error3");
+
     if (regex_is) {
-      valid = true;
-      formData[2].style.borderColor = "green";
+        valid = true;
+        formData[2].style.borderColor = "green";
     } else {
-      valid = false;
-      formData[2].style.borderColor = "red";
-      formData[2].insertAdjacentHTML("afterend", error);
+        valid = false;
+        formData[2].style.borderColor = "red";
+        formData[2].insertAdjacentHTML("afterend", error);
     } try {
-      errorData.remove();
+        errorData.remove();
     } catch {}
-    return valid;
+        return valid;
   }
 
-  // validation de la date de naissance
-  function birthdateData(data) {
+// validation de la date de naissance
+function birthdateData(data) {
     var valid = false;
     const regex_date = RegExp(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/);
     const regex_is = regex_date.test(data);
     var error = ('<span id="error4" class="msg_error">Vous devez entrer votre date de naissance valide.</span>');
     var errorData = document.getElementById("error4");
+
     if (regex_is) {
-      valid = true;
-      formData[3].style.borderColor = "green";
+        valid = true;
+        formData[3].style.borderColor = "green";
     }
     else {
-      valid = false
-      formData[3].style.borderColor = "red";
-      formData[3].insertAdjacentHTML("afterend", error);
+        valid = false
+        formData[3].style.borderColor = "red";
+        formData[3].insertAdjacentHTML("afterend", error);
     } try {
-      errorData.remove();
+        errorData.remove();
     } catch {}
-    return valid;
-  }
+        return valid;
+}
 
-  // validation du nombre de tournois disputés
-  function quantityData(data) {
+// validation du nombre de tournois disputés
+function quantityData(data) {
     var valid = false;
     const regex_quantity = RegExp(/^\d{1,}$/);
     const regex_is = regex_quantity.test(data);
     var error = ('<span id="error5" class="msg_error">Vous devez entrer un nombre.</span>');
     var errorData = document.getElementById("error5");
-    if (regex_is) {
-      valid = true;
-      formData[4].style.borderColor = "green";
-    } else {
-      valid = false;
-      formData[4].style.borderColor = "red";
-      formData[4].insertAdjacentHTML("afterend", error);
-    } try {
-      errorData.remove();
-    } catch {}
-    return valid;
-  }
 
-  // validation de la ville
-  function locationData(data) {
-    var valid = true;
-    for (element in  data) {
-      if (data[element].checked = false) {
+    if (regex_is) {
         valid = true;
-      }
+        formData[4].style.borderColor = "green";
+    } else {
+        valid = false;
+        formData[4].style.borderColor = "red";
+        formData[4].insertAdjacentHTML("afterend", error);
+    } try {
+        errorData.remove();
+    } catch {}
+        return valid;
+}
+
+// validation de la ville
+function locationData(data) {
+    var valid = true;
+
+    for (element in  data) {
+        if (data[element].checked = false) {
+            valid = true;
+        }
     };
+
     var error = ('<span id="error6" class="msg_error">Vous devez choisir une ville.</span>');
     var errorData = document.getElementById("error6");
+
     if (valid == true) {
     } else {
-      locationDiv.insertAdjacentHTML("afterend", error);
+        locationDiv.insertAdjacentHTML("afterend", error);
     } try {
-      errorData.remove();
+        errorData.remove();
     } catch {}
-    return valid;
-  }
+        return valid;
+}
 
-  // validation des conditions d'utilisation
-  function conditionData(data) {
+// validation des conditions d'utilisation
+function conditionData(data) {
     var error = ('<span id="error7" class="msg_error">Vous devez accepter les termes et conditions d’utilisation.</span>');
     var errorData = document.getElementById("error7");
+
     if (data.checked == true) {
-      valid = true;
+        valid = true;
     } else {
-      valid = false;
-      formData[12].insertAdjacentHTML("beforebegin", error);
+        valid = false;
+        formData[12].insertAdjacentHTML("beforebegin", error);
     } try {
-      errorData.remove();
+        errorData.remove();
     } catch {}
-    return valid;
-  }
+        return valid;
+}
+
+    
